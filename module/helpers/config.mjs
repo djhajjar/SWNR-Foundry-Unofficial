9c47cffc66d2e9ac7d11103de65339cb3d31169b
Daniel Hajjar <djhajjar@gmail.com>
2021-12-08 07:38:23 -0600

    Init

---
+++ module/helpers/config.mjs
@@ -0,0 +1,29 @@
+export const SWNR = {};
+
+// Define constants here, such as:
+SWNR.foobar = {
+  'bas': 'SWNR.bas',
+  'bar': 'SWNR.bar'
+};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ SWNR.abilities = {
+  "str": "SWNR.AbilityStr",
+  "dex": "SWNR.AbilityDex",
+  "con": "SWNR.AbilityCon",
+  "int": "SWNR.AbilityInt",
+  "wis": "SWNR.AbilityWis",
+  "cha": "SWNR.AbilityCha"
+};
+
+SWNR.abilityAbbreviations = {
+  "str": "SWNR.AbilityStrAbbr",
+  "dex": "SWNR.AbilityDexAbbr",
+  "con": "SWNR.AbilityConAbbr",
+  "int": "SWNR.AbilityIntAbbr",
+  "wis": "SWNR.AbilityWisAbbr",
+  "cha": "SWNR.AbilityChaAbbr"
+};
